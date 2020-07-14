7202f92e9af781821f12f51d3f05d94b28fcf37b
Augusto Sopelsa Klaic <augusto.klaic@acad.pucrs.br>
2020-07-13 21:31:17 -0300

    Update configStressTest.sh

--- petclinic-spring5-reactive/configStressTest.sh
+++ petclinic-spring5-reactive/configStressTest.sh
@@ -1,12 +1,12 @@
 version=2.3.1;
 
-#wget https://repo1.maven.org/maven2/io/gatling/highcharts/gatling-charts-highcharts-bundle/$version/gatling-charts-highcharts-bundle-$version-bundle.zip;
+wget https://repo1.maven.org/maven2/io/gatling/highcharts/gatling-charts-highcharts-bundle/$version/gatling-charts-highcharts-bundle-$version-bundle.zip;
 unzip gatling-charts-highcharts-bundle-$version-bundle.zip;
 cp -a stressTest/. gatling-charts-highcharts-bundle-$version/user-files/simulations/computerdatabase/;
 cd gatling-charts-highcharts-bundle-$version/bin/;
-#./gatling.sh -s computerdatabase.PetClinicRouterOwnerStressTest;ok
-#./gatling.sh -s computerdatabase.PetClinicRouterVetsStressTest;ok
-#./gatling.sh -s computerdatabase.PetClinicRouterSpecilitiesStressTest;ok
-#./gatling.sh -s computerdatabase.PetClinicRouterPetTypesStressTest;ok
-#./gatling.sh -s computerdatabase.PetClinicRouterPetsStressTest;ok
+./gatling.sh -s computerdatabase.PetClinicRouterOwnerStressTest;
+./gatling.sh -s computerdatabase.PetClinicRouterVetsStressTest;
+./gatling.sh -s computerdatabase.PetClinicRouterSpecilitiesStressTest;
+./gatling.sh -s computerdatabase.PetClinicRouterPetTypesStressTest;
+./gatling.sh -s computerdatabase.PetClinicRouterPetsStressTest;
 ./gatling.sh -s computerdatabase.PetClinicRouterVisitsStressTest;
